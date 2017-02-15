8cf175a4775d04b8341067366eda42b174d1884d
José Cabarcas <josecabarcas94@gmail.com>
2017-02-14 21:28:48 -0500

    hello world

---
+++ app/Http/Controllers/CubeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CubeController extends Controller
+{
+    //
+    public function index()
+    {
+    	return view('home');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+
+<p>Hola mundo</p>
+
+@endsection
